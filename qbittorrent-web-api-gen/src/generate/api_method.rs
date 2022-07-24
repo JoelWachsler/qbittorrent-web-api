c1eeb399a0b7540bf064bdf75c7bed907369edb7
Joel Wachsler <JoelWachsler@users.noreply.github.com>
2022-07-24 00:02:57 +0000

    Support for adding derives

--- qbittorrent-web-api-gen/src/generate/api_method.rs
+++ qbittorrent-web-api-gen/src/generate/api_method.rs
@@ -1,30 +1,11 @@
 use case::CaseExt;
-use proc_macro2::{Ident, TokenStream};
-use quote::quote;
+use proc_macro2::Ident;
 
 use crate::parser;
 
 use super::util;
 
 impl parser::ApiMethod {
-    pub fn structs(&self) -> TokenStream {
-        let objects = self.types.objects();
-        let structs = objects.iter().map(|obj| obj.generate_struct());
-
-        quote! {
-            #(#structs)*
-        }
-    }
-
-    pub fn enums(&self) -> TokenStream {
-        let enums = self.types.enums();
-        let generated_enums = enums.iter().map(|e| e.generate());
-
-        quote! {
-            #(#generated_enums)*
-        }
-    }
-
     pub fn name_snake(&self) -> Ident {
         util::to_ident(&self.name.to_snake())
     }
--- qbittorrent-web-api-gen/src/generate/mod.rs
+++ qbittorrent-web-api-gen/src/generate/mod.rs
@@ -15,12 +15,13 @@ use self::{group::generate_groups, skeleton::generate_skeleton};
 
 pub fn generate(ast: &syn::DeriveInput, api_content: &str) -> TokenStream {
     let ident = &ast.ident;
+    let resp_derives = get_response_derives(ast);
 
     let token_tree = md_parser::TokenTreeFactory::create(api_content);
     let api_groups = parser::parse_api_groups(token_tree);
 
     let skeleton = generate_skeleton(ident);
-    let groups = generate_groups(api_groups);
+    let groups = generate_groups(api_groups, resp_derives);
     let impl_ident = syn::Ident::new(&format!("{}_impl", ident).to_snake(), ident.span());
 
     quote! {
@@ -29,4 +30,35 @@ pub fn generate(ast: &syn::DeriveInput, api_content: &str) -> TokenStream {
             #groups
         }
     }
+}
+
+fn get_response_derives(ast: &syn::DeriveInput) -> Vec<String> {
+    ast.attrs
+        .iter()
+        .find(|attr| {
+            attr.path
+                .get_ident()
+                .map(|ident| ident == "api_gen")
+                .unwrap_or(false)
+        })
+        .into_iter()
+        .flat_map(|attr| attr.parse_meta().ok())
+        .filter_map(|meta| match meta {
+            syn::Meta::List(list) => Some(list.nested),
+            _ => None,
+        })
+        .flat_map(|nested| nested.into_iter())
+        .filter_map(|value| match value {
+            syn::NestedMeta::Meta(meta) => Some(meta),
+            _ => None,
+        })
+        .filter_map(|value| match value {
+            syn::Meta::NameValue(name_value) => Some(name_value.lit),
+            _ => None,
+        })
+        .filter_map(|lit| match lit {
+            syn::Lit::Str(str) => Some(str.value().split(',').map(|s| s.trim()).collect()),
+            _ => None,
+        })
+        .collect()
 }
